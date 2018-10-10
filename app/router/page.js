77357f83e4f44b695e2f523da78ba0b128b623e3
liudeng <deng.liu@tongdun.cn>
2018-10-10 15:24:44 +0800

    提交

---
+++ app/router/page.js
@@ -0,0 +1,8 @@
+module.exports = app => {
+    const checkLogin = app.middleware.checkLogin({});
+    const prefix="/page";
+  
+    // app.router.post(prefix + '/findOrCreate', app.controller.page.findOrCreate);
+    
+  };
+  
