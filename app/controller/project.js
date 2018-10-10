a392e7247424a955b980a1809bda34fee7128a24
liudeng <deng.liu@tongdun.cn>
2018-10-10 15:22:30 +0800

    提交

---
+++ app/controller/project.js
@@ -0,0 +1,54 @@
+const Controller = require('egg').Controller;
+const { request, summary, query, path, body, tags ,description,responses}=require('egg-swagger-decorator');
+const group = tags(['项目管理']);
+
+
+class ProjectController extends Controller {
+    constructor(ctx) {
+        super(ctx);
+        this.session = ctx.session;
+    }
+
+    
+    async index() {
+        const ctx = this.ctx;
+        const { name, query, role } = ctx.session;
+        const querys = {
+            creator: name,
+            role,
+            page: ctx.helper.parseInt(query.page),
+            pageSize: ctx.helper.parseInt(query.pageSize)
+        }
+        ctx.body = await ctx.service.project.list(querys);
+    }
+
+    async create() {
+        const ctx = this.ctx;
+        const { name, name_cn = name } = ctx.request.body;
+        const project_id = ctx.helper.hashCode(name + Date.now().toString());
+        if (!ctx.helper.isNotEmpty(name)) {
+            ctx.body = ctx.response.ServerResponse.error('参数不合法');
+        }
+        ctx.body = await ctx.service.project.create({ name: trim(name), name_cn: trim(name_cn), project_id });
+    }
+
+    async destory() {
+        const ctx = this.ctx;
+        const { id } = ctx.params;
+        ctx.body = await ctx.service.project.destory(id);
+    }
+
+    async update() {
+        const ctx = this.ctx;
+        const id = ctx.helper.parseInt(ctx.params.id);
+        const { name } = ctx.request.body;
+        if (!ctx.helper.isNotEmpty(name)) {
+            ctx.body = ctx.response.ServerResponse.error('参数不合法');
+        }
+        ctx.body = await ctx.service.project.update({id, name: trim(name)});
+    }
+
+    
+}
+
+module.exports = ProjectController;
